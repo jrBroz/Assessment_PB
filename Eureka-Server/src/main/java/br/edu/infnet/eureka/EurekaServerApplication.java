aefcf94c8bf2a497ae7d1c838c3feb12a85db2e8
Joao Rafael <joaorafael1344@gmail.com>
2026-06-03 10:55:18 -0300

    mudanca pom.xml e application.yml para suportar eureka

--- Eureka-Server/src/main/java/br/edu/infnet/eureka/EurekaServerApplication.java
+++ Eureka-Server/src/main/java/br/edu/infnet/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package main.java.br.edu.infnet.eureka;
+package br.edu.infnet.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
--- Order-Service/src/main/java/br/edu/infnet/order/OrderApplication.java
+++ Order-Service/src/main/java/br/edu/infnet/order/OrderApplication.java
@@ -1,5 +1,4 @@
 package br.edu.infnet.order;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
