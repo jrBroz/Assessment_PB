587115a42c8ef17e11307b049f8c1f2491ede31b
Joao Rafael <joaorafael1344@gmail.com>
2026-06-14 15:31:14 -0300

    estrutura de observabilidade com prometheus e grafana, falta apenas corrigir os graficos

--- Product-Service/src/main/java/br/edu/infnet/product/ProductApplication.java
+++ Product-Service/src/main/java/br/edu/infnet/product/ProductApplication.java
@@ -1,5 +1,4 @@
 package br.edu.infnet.product;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
